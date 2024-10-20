853e5827179908a93e5bd6518e3a030d9fe4413d
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-10-20 19:35:20 +0700

    Move field price_code from movie to rental

--- movie.py
+++ movie.py
@@ -1,28 +1,18 @@
 import logging
-import enum
-from pricing import NEW_RELEASE, REGULAR, CHILDREN, PriceStrategy
-    
+import enum    
 
 class Movie:
     """
     A movie available for rent.
     """
-    # The types of movies (price_code). 
-    REGULAR = REGULAR
-    NEW_RELEASE = NEW_RELEASE
-    CHILDRENS = CHILDREN
-    
-    def __init__(self, title, price_code):
+        
+    def __init__(self, title):
         # Initialize a new movie. 
         self.title = title
-        self.price_code = price_code
-
-    def get_price_code(self) -> PriceStrategy:
-        # get the price code
-        return self.price_code
     
     def get_title(self):
         return self.title
     
     def __str__(self):
         return self.title
+    
--- rental.py
+++ rental.py
@@ -1,5 +1,6 @@
 from movie import Movie
 import logging
+from pricing import NEW_RELEASE, REGULAR, CHILDREN, PriceStrategy
 
 class Rental:
    """
@@ -11,13 +12,23 @@ class Rental:
    rental period is calculated.
    For simplicity of this application only days_rented is recorded.
    """
+   
+   # The types of movies (price_code). 
+   REGULAR = REGULAR
+   NEW_RELEASE = NEW_RELEASE
+   CHILDRENS = CHILDREN
     
-   def __init__(self, movie, days_rented): 
+   def __init__(self, movie, days_rented, price_code): 
       """Initialize a new movie rental object for
          a movie with known rental period (daysRented).
       """
       self.movie = movie
       self.days_rented = days_rented
+      self.price_code = price_code
+      
+   def get_price_code(self) -> PriceStrategy:
+      # get the price code
+      return self.price_code
 
    def get_movie(self) -> Movie:
       return self.movie
@@ -26,8 +37,8 @@ class Rental:
       return self.days_rented
   
    def get_price(self) -> float:
-      return self.get_movie().get_price_code().get_price(self.get_days_rented())
+      return self.get_price_code().get_price(self.get_days_rented())
      
    def rental_point(self):
-      return self.get_movie().get_price_code().get_rental_points(self.get_days_rented())
+      return self.get_price_code().get_rental_points(self.get_days_rented())
         
